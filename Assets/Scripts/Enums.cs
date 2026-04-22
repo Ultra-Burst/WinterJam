37f4dc7b61415b82910c5910b347fcb631e3b4a3
NotYourAvSalarian <notableai@gmail.com>
2026-04-22 13:34:38 -0400

    Placeholder enum values

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public enum Food
 {
-
+    Food1 = 0,
+    Food2 = 1,
+    Food3 = 2,
+    Food4 = 3
 }
 public enum Hobby
 {
-
+    Hobby1 = 0,
+    Hobby2 = 1,
+    Hobby3 = 2,
+    Hobby4 = 3
 }
 
 public enum Job
@@ -22,19 +28,30 @@ public enum Job
 
 public enum Education
 {
-
+    Education1 = 0,
+    Education2 = 1,
+    Education3 = 2,
+    Education4 = 3
 }
 
 public enum Origin
 {
-
+    Origin1 = 0,
+    Origin2 = 1,
+    Origin3 = 2,
+    Origin4 = 3
 }
 
 public enum Personality
 {
-
+    Personality1 = 0,
+    Personality2 = 1,
+    Personality3 = 2
 }
 public enum Creature
 {
-
+    Creature1 = 0,
+    Creature2 = 1,
+    Creature3 = 2,
+    Creature4 = 3
 }
